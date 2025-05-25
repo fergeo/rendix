fa562a2fbf63d07ed3a07c5f3a648aa1e71dce0b
fergeo <fergeo@yahoo.com>
2025-05-24 23:00:21 -0300

    Alta Inscripcion
    Alta Inscripcion

--- routes/admin.js
+++ routes/admin.js
@@ -16,6 +16,8 @@ router.get('/', (req, res) => {
   res.render('admin/menu', { usuario: req.session?.usuario || 'Admin' });
 });
 
+
+//CURSOS
 // Submenú o índice de gestión de cursos
 router.get('/cursos', (req, res) => {
   res.render('admin/cursos');
@@ -30,10 +32,21 @@ router.get('/cursos/baja', mostrarBaja);
 router.post('/cursos/borrar', borrarCursos);
 
 // Modificación de curso
-router.get('/cursos/modificar', mostrarModificar);
+router.get('/modificacion-curso', mostrarModificar);
 router.post('/cursos/modificar', modificarCurso);
 
 // Consulta de cursos
 router.get('/cursos/consulta', listarCursos);
+
+
+//INSCRIPCIONES
+router.get('/inscripciones', (req, res) => {
+  res.render('admin/inscripciones'); // archivo views/admin/menuInscripciones.pug
+});
+
+router.get('/altaInscripcion', (req, res) => {
+  // Aquí deberías cargar los datos y renderizar la vista
+  res.render('admin/altaInscripcion', { cursos: [], inscripciones: [] });
+});
 
 export default router;
