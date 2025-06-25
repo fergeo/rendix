223cbfc9ac861c100180d5263a3b24ceab1bbc2b
fergeo <fergeo@yahoo.com>
2025-06-25 20:33:08 -0300

    Correciones
    Correciones

--- models/admin/Inscripcion.js
+++ models/admin/Inscripcion.js
@@ -1,11 +1,11 @@
-// models/admin/Inscripcion.js
 import mongoose from 'mongoose';
 
 const inscripcionSchema = new mongoose.Schema({
     studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Alumno', required: true },
     courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Curso', required: true },
 }, {
-  timestamps: true, // Opcional, para tener createdAt y updatedAt
+  timestamps: true,       // para createdAt y updatedAt
+  collection: 'inscripciones'  // nombre explícito de la colección en MongoDB Atlas
 });
 
 const Inscripcion = mongoose.model('Inscripcion', inscripcionSchema);
